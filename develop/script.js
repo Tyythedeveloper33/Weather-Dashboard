86ee266720a1845d17426fb278abde5886cd983d
unknown <kingwebbe@gmail.com>
2023-07-09 00:20:25 -0400

    added weather api need touch up

--- develop/script.js
+++ develop/script.js
@@ -1,12 +1,12 @@
 //geocoding api
 
 
-//weather api
-`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7294cd9f83d12475284a0873d57ce046`
+
 // geocoding api
 // create add event listener
-var searchBtn = document.getElementById("search-button")
-searchBtn.addEventListener('click', searchCity)
+var searchBtn = document.getElementById("search-button");
+searchBtn.addEventListener('click', searchCity);
+// searchcity function
 function searchCity() {
     var cityName = document.querySelector('.search-input').value
     // statecode & countryCode is replaced with the appropriate state code & countryCode  if applied
@@ -22,15 +22,17 @@ function searchCity() {
     fetch(geocodingUrl)
          .then(response => response.json())
          .then(data => {
-            //handle the APi response and update the content containers
-var content1 = document.getElementById('content-container1');
-var content2 = document.getElementById('content-container2');
-
-
+         
 // update content with api data
-if (data.length > 0) {
-    var cityData = data[0];
-   console.log(`city: ${cityData.name}, Latitude: ${cityData.lat}, longitude: ${cityData.lon}`);
+          if (data.length > 0) {
+          var cityData = data[0];
+          var Latitude = cityData.lat;
+          var longitude = cityData.lon
+          console.log(`city: ${cityData.name}, Latitude: ${cityData.lat}, longitude: ${cityData.lon}`);
+          //weather api
+          var weatherUrl =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${7294cd9f83d12475284a0873d57ce046}`;
+
+
 
 } else {
     console.log('no results found.');
@@ -40,5 +42,23 @@ if (data.length > 0) {
          .catch(error => {
             console.error("Error:", error);
          });
-            
+     .then(response => response.json())  
+     .then(weatherData => {
+        // handle weather api response and update content container
+        var content1 = document.getElementById('content-container1');
+        var content2 = document.getElementById('content-container2');
+        //clear previous content
+        content1.innerHTML = '';
+        content2.innerHTML = '';
+        // update content with weather data
+        console.log(weatherData);
+        //log the weather data  to the console
+        .catch(error => {
+            console.error("Error:", error);
+        });
+
+     }
+
+        );
+
 }
