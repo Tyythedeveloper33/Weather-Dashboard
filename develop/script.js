642ee17455d9763061cbd2bea8afd306cfd5a59a
unknown <kingwebbe@gmail.com>
2023-07-09 18:40:44 -0400

    added json object for weather api and converted temp from kelvin to farenheight

--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,4 @@
-//geocoding api
+
 
 
 
@@ -8,7 +8,7 @@ var searchBtn = document.getElementById("search-button");
 searchBtn.addEventListener('click', searchCity);
 // searchcity function
 function searchCity() {
-    var cityName = document.querySelector('.search-input').value
+    var cityName = document.querySelector('.search-input').value;
     // statecode & countryCode is replaced with the appropriate state code & countryCode  if applied
     var stateCode = '';
     var countryCode = '';
@@ -27,38 +27,46 @@ function searchCity() {
           if (data.length > 0) {
           var cityData = data[0];
           var Latitude = cityData.lat;
-          var longitude = cityData.lon
-          console.log(`city: ${cityData.name}, Latitude: ${cityData.lat}, longitude: ${cityData.lon}`);
+          var longitude = cityData.lon;
           //weather api
-          var weatherUrl =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${7294cd9f83d12475284a0873d57ce046}`;
-
-
-
-} else {
-    console.log('no results found.');
-}
-
-         })
-         .catch(error => {
-            console.error("Error:", error);
-         });
-     .then(response => response.json())  
-     .then(weatherData => {
-        // handle weather api response and update content container
+          var weatherUrl =`https://api.openweathermap.org/data/2.5/forecast?lat=${Latitude}&lon=${longitude}&appid=${apiKey}`;
+          console.log(`city: ${cityData.name}, Latitude: ${cityData.lat}, longitude: ${cityData.lon}`);
+         fetch(weatherUrl)
+         .then(response => response.json())  
+        .then(weatherData => {
+             // handle weather api response and update content container
         var content1 = document.getElementById('content-container1');
         var content2 = document.getElementById('content-container2');
-        //clear previous content
-        content1.innerHTML = '';
-        content2.innerHTML = '';
+       
+        content2.createElement()
         // update content with weather data
         console.log(weatherData);
-        //log the weather data  to the console
+        var firstForecast = weatherData.list[0];
+        var temperature = firstForecast.main.temp;
+        var description = firstForecast.weather[0].description;
+        
+        console.log('Description:', description);
+         temperatureFarenheight = (temperature - 273.15) * 1.8 + 32
+         console.log('Temperature:', temperatureFarenheight);
+        })
         .catch(error => {
             console.error("Error:", error);
         });
 
-     }
+           } else {
+           throw new Error('No results found.');
+       }
+    })
+       .catch(error => {
+        console.error("Error:", error);
 
-        );
+         });
+         
+       
+  }
 
-}
+       
+
+     
+
+      
